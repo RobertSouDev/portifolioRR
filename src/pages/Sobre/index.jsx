4f99e33c32b0968b3c27ded8cbbbae9bd0c5fc80
RobertSouDev <rogerhallks1@gmail.com>
2024-07-22 23:54:03 -0300

    reposividade do sobre

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -1,16 +1,18 @@
 function Sobre() {
     return ( 
-        <div className="w-full h-[55rem] flex items-center justify-center">
-            <div className=" w-[80%] h-[80%] flex">
-                <div className=" w-[50%] h-[100%] flex justify-center items-center">
+        <div className="w-full h-auto flex flex-col md:flex-row items-center justify-center">
+            <div className="w-[80%] h-auto md:h-[80%] flex flex-col md:flex-row">
+                {/* Primeira Div */}
+                <div className="w-full md:w-[50%] h-[100%] flex justify-center items-center">
                     <div className="w-[50%] h-[80%] pt-10">
                         <img src="https://via.placeholder.com/300" alt="Placeholder" />
                     </div>
                 </div>
-                <div className=" w-[50%] h-[100%] flex items-center justify-center p-8">
-                    <div className="max-w-4xl mx-auto space-y-6">
+                {/* Segunda Div */}
+                <div className="w-full md:w-[50%] h-[100%] flex items-center justify-center p-8 mt-4 md:mt-0">
+                    <div>
                         <h1 className="text-4xl font-bold">Robert Roger</h1>
-                        <h2 className="text-2xl font-semibold">SOBRE </h2>
+                        <h2 className="text-2xl font-semibold">SOBRE</h2>
                         <p className="text-lg">
                             Sou um parágrafo. Clique aqui para editar e adicionar o seu próprio texto. É fácil! Basta clicar em "Editar Texto" ou clicar duas vezes sobre mim e você poderá adicionar o seu próprio conteúdo e trocar fontes. Sou um ótimo lugar para você contar sua história e permitir que seus clientes saibam um pouco mais sobre você.
                         </p>
