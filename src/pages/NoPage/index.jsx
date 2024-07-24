285127624bbb47ece2fc5cc011d261a906d9a591
RobertSouDev <rogerhallks1@gmail.com>
2024-07-23 21:45:25 -0300

    teste

--- src/pages/NoPage/index.jsx
+++ src/pages/NoPage/index.jsx
@@ -1,6 +1,6 @@
 function NoPage() {
     return ( 
-        <div className="  w-full h-[75vh] flex items-center justify-center">
+        <div className="  w-full h-[55rem] flex items-center justify-center">
             <h1>Not Found</h1>
         </div>
      );
